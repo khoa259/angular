aabbe3035263aaa92739c6a4287cd9a4c6eb9e9d
khoanh <khoa10688@gmail.com>
2022-05-17 23:22:34 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assm';
+  name = 'khoanh'
+  class = 'ph16639'
+
+  teachers = [
+    {
+      "id": 1,
+      "name": "Nguyễn Hữu Khoa",
+      "age": 27,
+      "gender": 1,
+      "avatar": "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
+      "status" : 1
+    },
+    {
+      "id": 2,
+      "name": "Trịnh Hòa An",
+      "age": 20,
+      "gender": 0,
+      "avatar": "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
+      "status" : 0
+    },
+    {
+      "id": 3,
+      "name": "Nguyễn Hữu Khoa",
+      "age": 30,
+      "gender": 1,
+      "avatar": "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
+      "status" : 1
+    }
+  ];
+  studentName = 'anth khoanh'
+  
+  studentId = '16639'  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
+import { NameComponent } from './name/name.component';
+import { IdentityComponent } from './identity/identity.component';
+import { TableNameComponent } from './table/table-name/table-name.component';
+import { TableAvatarComponent } from './table/table-avatar/table-avatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    NameComponent,
+    IdentityComponent,
+    TableNameComponent,
+    TableAvatarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
